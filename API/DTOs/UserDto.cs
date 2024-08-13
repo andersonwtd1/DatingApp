2978341b7105fec36ab9d5f3322b87a659664a7f
andersonwtd1 <andersonwtd1@gmail.com>
2024-08-13 11:17:29 +0100

    Complete work to support bearer tokens in API.

---
+++ API/DTOs/UserDto.cs
@@ -0,0 +1,11 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace API.DTOs
+{
+    public class UserDto
+    {
+        public required string UserName { get; set; }
+
+        public required string Token { get; set; }
+    }
+}
